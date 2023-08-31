3875dd237e128e7861e883b215b48d478990bdb5
Bomowo <oeccc25@yahoo.com>
2023-08-31 14:42:56 +0300

    Simple styling and test API calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
+import { useEffect, useState } from "react"
 
 function App() {
 
-  return (
-    <>
+  const [data, setData] = useState(null)
 
-    </>
+  useEffect(()=>{
+    fetch('https://opentdb.com/api.php?amount=5&category=26&type=multiple')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  },[])
+
+  function clickHandle () {
+    console.log(data.results)
+  }
+
+  return (
+    <div className="start-page">
+      <h1>Quizzical</h1>
+      <p>Some description if needed</p>
+      <button onClick={clickHandle}>Start quiz</button>
+    </div>
   )
 }
 
